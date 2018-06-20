8ffe2629ccd97275127dc834a04936b27683b5e6
LarryGF <larrywtf609@gmail.com>
2018-06-20 17:13:38 -0400

    migrated all the logic to functions.py

--- functions.py
+++ functions.py
@@ -1,15 +1,220 @@
 import json
-dictionary = {'criticidad': [1, ], 'eficiencia': [5, 1, 2, 4], 'agilidad': [
-	3, 2, 4, 5], 'innovacion': [3, 2, 5, 1], 'criterio': [5, 4, 5, 2, 1, 4, 3, 2, 5, 1, 4]}
+import os
 
-try:
-	file = open('users.json')
-	# exisiting_users = json.load(file)
 
-except:
-	file = open('users.json', 'w')
-	file.write("{}")
+os.makedirs('data', exist_ok=True)
+data_dir = os.path.join(os.path.abspath('.'),'data')
+
+
+def to_list(dic,list_to_send):
+
+	lista = []
+	for fila in dic.keys():
+		lista.append(dic[fila])
+	list_to_send.append(lista)	
+	return lista
+
+
+def func_save(table,lista,last_id):
+	dic = {}
+	file = os.path.join(data_dir,table+'.json')
+	file = open(file,'w')
+	try:
+		for row in lista:
+			dic[row['id']] = row
+
+		json.dump(dic,file)
+
+		file2 = os.path.join(data_dir,'last.json')
+		file2 = open(file2,'w')
+		dic = {'last_id':last_id}
+		json.dump(dic,file2)
+		return 'Success'
+	except Exception as e:
+		return e
+
+
+def func_load():
+	list_to_send = []
+	file = os.path.join(data_dir,'objective.json')
+	try:
+		file = open(file)
+		dic = json.load(file)
+		file.close()
+		to_list(dic,list_to_send)
+		
+	except:
+		list_to_send.append([])
+
+
+	file = os.path.join(data_dir,'criterion.json')
+	try:
+		file = open(file)
+		dic = json.load(file)
+		file.close()
+		to_list(dic,list_to_send)
+	except:
+		list_to_send.append([])
+	
+
+
+	file = os.path.join(data_dir,'last.json')
+	try:
+		file = open(file)
+		dic = json.load(file)
+		file.close()
+		last_id = dic['last_id']
+		list_to_send.append(last_id)
+	except:
+		list_to_send.append(0)
+	return list_to_send
+
+
+def func_del(lista,last_id):
+	file = os.path.join(data_dir,'objective.json')
+	try:
+		file = open(file)
+	except:
+		return 'You must add some data before you can delete it :)'
+	objective = json.load(file)
 	file.close()
+
+	file = os.path.join(data_dir,'criterion.json')
+	try:
+		file = open(file)
+	except:
+		return 'You must add some data before you can delete it :)'
+	
+	criterion = json.load(file)
+	file.close()
+
+	
+	if len(lista) != 0 and str(lista[0]['id']) in objective.keys():
+		
+		for dic in lista:
+			
+			objective.pop(str(dic['id']))
+
+		objective = to_list(objective,[])
+		
+
+		return 'objective',objective
+
+
+	elif len(lista) !=0 and str(lista[0]['id']) in criterion.keys():
+
+		for dic in lista:
+			
+			criterion.pop(str(dic['id']))
+
+		criterion = to_list(criterion,[])
+		
+
+		return 'criterion',criterion
+
+	else:
+		return 'It seems like I screwed up, send me an email so I can fix it'
+
+
+def func_check(users,table_obj,table_crit,last_id):
+	if users == 0 or users == None or users == '':
+		return "You must have at least one user"
+	list_obj = []
+	list_crit = []
+
+	for row in table_obj:
+		try:
+			int(row['crit1'])
+		except:
+			row['crit1']=0
+		try:
+			int(row['crit2'])
+		except:
+			row['crit2']=0
+		try:
+			int(row['crit3'])
+		except:
+			row['crit3']=0
+		try:
+			int(row['crit4'])
+		except:
+			row['crit4']=0
+		try:
+			int(row['crit5'])
+		except:
+			row['crit5']=0
+
+			
+
+		if int(row['crit1'])+int(row['crit2'])+int(row['crit3'])+int(row['crit4'])+int(row['crit5']) > int(users):
+			list_obj.append(row)
+		else:
+			pass
+
+
+	for row in table_crit:
+		try:
+			int(row['crit1'])
+		except:
+			row['crit1']=0
+		try:
+			int(row['crit2'])
+		except:
+			row['crit2']=0
+		try:
+			int(row['crit3'])
+		except:
+			row['crit3']=0
+		try:
+			int(row['crit4'])
+		except:
+			row['crit4']=0
+		try:
+			int(row['crit5'])
+		except:
+			row['crit5']=0
+
+		if int(row['crit1'])+int(row['crit2'])+int(row['crit3'])+int(row['crit4'])+int(row['crit5']) > int(users):
+			list_crit.append(row)
+		else:
+			pass
+	if len(list_obj) != 0:
+		var = func_del(list_obj,last_id)
+		var = var[1]
+		print(var)
+		mistake=True
+	elif len(list_crit) != 0:
+		var2 = func_del(list_crit,last_id)
+		var2 = var2[1]
+		print(var2)
+		mistake=True
+
+	else:
+		var = table_obj
+		var2 = table_crit
+		mistake = False
+
+	var2 = table_crit
+	return var,var2,mistake
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def calculate_limits(exisiting_users):
@@ -18,35 +223,9 @@ def calculate_limits(exisiting_users):
 			  'third': val*3, 'fourth': val*4, 'fifth': val*5}
 	return limits
 
-def remove_user(user):
-	try:	
-		existing_users = load_data()
-		existing_users.pop(user)
-		save_data(existing_users)
-	except Exception as e:
-		return e
-	return 'Succes'
-	
-def new_user(name: str, dic: dict):
-	existing_users= load_data()
-	existing_users[name] = dic
-	# print(existing_users)
-	# return exisiting_users
-	save_data(existing_users)
-	return 'Success'
 
 
-def load_data():
-	file = open('users.json')
-	users = json.load(file)
-	return users
 
-
-def save_data(exisiting_users):
-	file = open('users.json', 'w')
-	json.dump(exisiting_users, file)
-	file.close()
-	return "Success"
 
 
 def calculate_element(table: str, aspect: int, dic: dict, limits: dict):
@@ -106,6 +285,7 @@ def calculate_total(users, limits):
 		final_result[categoria] = result_list
 	# print(final_result)
 	return final_result
+
 
 def send_table():
 	list_to_send = []
--- likert.py
+++ likert.py
@@ -3,203 +3,29 @@ import json
 import os
 from functions import *
 
-os.makedirs('data', exist_ok=True)
-data_dir = os.path.join(os.path.abspath('.'),'data')
-
-
-
 eel.init('web')
-def to_list(dic,list_to_send):
-
-	lista = []
-	for fila in dic.keys():
-		lista.append(dic[fila])
-	list_to_send.append(lista)	
-	return lista
 
 
 @eel.expose
 def save(table,lista,last_id):
-	dic = {}
-	file = os.path.join(data_dir,table+'.json')
-	file = open(file,'w')
-	try:
-		for row in lista:
-			dic[row['id']] = row
-
-		json.dump(dic,file)
-
-		file2 = os.path.join(data_dir,'last.json')
-		file2 = open(file2,'w')
-		dic = {'last_id':last_id}
-		json.dump(dic,file2)
-		return 'Success'
-	except Exception as e:
-		return e
+	result = func_save(table,lista,last_id)
+	return result
+	
 
 @eel.expose
 def load():
-	list_to_send = []
-	file = os.path.join(data_dir,'objective.json')
-	try:
-		file = open(file)
-		dic = json.load(file)
-		file.close()
-		to_list(dic,list_to_send)
-		
-	except:
-		list_to_send.append([])
-
-
-	file = os.path.join(data_dir,'criterion.json')
-	try:
-		file = open(file)
-		dic = json.load(file)
-		file.close()
-		to_list(dic,list_to_send)
-	except:
-		list_to_send.append([])
-	
-
-
-	file = os.path.join(data_dir,'last.json')
-	try:
-		file = open(file)
-		dic = json.load(file)
-		file.close()
-		last_id = dic['last_id']
-		list_to_send.append(last_id)
-	except:
-		list_to_send.append(0)
-	return list_to_send
-
+	result = func_load()
+	return result
 @eel.expose
 def delitems(lista,last_id):
-	file = os.path.join(data_dir,'objective.json')
-	try:
-		file = open(file)
-	except:
-		return 'You must add some data before you can delete it :)'
-	objective = json.load(file)
-	file.close()
-
-	file = os.path.join(data_dir,'criterion.json')
-	try:
-		file = open(file)
-	except:
-		return 'You must add some data before you can delete it :)'
-	
-	criterion = json.load(file)
-	file.close()
+	result = func_del(lista,last_id)
+	return result
 
 	
-	if len(lista) != 0 and str(lista[0]['id']) in objective.keys():
-		
-		for dic in lista:
-			
-			objective.pop(str(dic['id']))
-
-		objective = to_list(objective,[])
-		
-
-		return 'objective',objective
-
-
-	elif len(lista) !=0 and str(lista[0]['id']) in criterion.keys():
-
-		for dic in lista:
-			
-			criterion.pop(str(dic['id']))
-
-		criterion = to_list(criterion,[])
-		
-
-		return 'criterion',criterion
-
-	else:
-		return 'It seems like I screwed up, send me an email so I can fix it'
-
 @eel.expose
 def check(users,table_obj,table_crit,last_id):
-	if users == 0 or users == None or users == '':
-		return "You must have at least one user"
-	list_obj = []
-	list_crit = []
-
-	for row in table_obj:
-		try:
-			int(row['crit1'])
-		except:
-			row['crit1']=0
-		try:
-			int(row['crit2'])
-		except:
-			row['crit2']=0
-		try:
-			int(row['crit3'])
-		except:
-			row['crit3']=0
-		try:
-			int(row['crit4'])
-		except:
-			row['crit4']=0
-		try:
-			int(row['crit5'])
-		except:
-			row['crit5']=0
-
-			
-
-		if int(row['crit1'])+int(row['crit2'])+int(row['crit3'])+int(row['crit4'])+int(row['crit5']) > int(users):
-			list_obj.append(row)
-		else:
-			pass
-
-
-	for row in table_crit:
-		try:
-			int(row['crit1'])
-		except:
-			row['crit1']=0
-		try:
-			int(row['crit2'])
-		except:
-			row['crit2']=0
-		try:
-			int(row['crit3'])
-		except:
-			row['crit3']=0
-		try:
-			int(row['crit4'])
-		except:
-			row['crit4']=0
-		try:
-			int(row['crit5'])
-		except:
-			row['crit5']=0
-
-		if int(row['crit1'])+int(row['crit2'])+int(row['crit3'])+int(row['crit4'])+int(row['crit5']) > int(users):
-			list_crit.append(row)
-		else:
-			pass
-	if len(list_obj) != 0:
-		var = delitems(list_obj,last_id)
-		var = var[1]
-		print(var)
-		mistake=True
-	elif len(list_crit) != 0:
-		var2 = delitems(list_crit,last_id)
-		var2 = var2[1]
-		print(var2)
-		mistake=True
-
-	else:
-		var = table_obj
-		var2 = table_crit
-		mistake = False
-
-	var2 = table_crit
-	return var,var2,mistake
-
+	result = func_check(users,table_obj,table_crit,last_id)
+	return result
+	
 
 eel.start('likert1.html')
