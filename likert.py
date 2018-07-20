b100e19edd63b4d960ccf4ffdc391521435ff51c
LarryGF <larrywtf609@gmail.com>
2018-07-20 05:58:43 -0400

    lets you save in .txt

--- likert.py
+++ likert.py
@@ -7,39 +7,56 @@ eel.init('web')
 
 
 @eel.expose
-def save(table,lista,last_id,values_list):
-	result = func_save(table,lista,last_id,values_list)
-	return result
-	
+def save(table, lista, last_id, values_list):
+    result = func_save(table, lista, last_id, values_list)
+    return result
+
 
 @eel.expose
 def load():
-	result = func_load()
-	return result
-@eel.expose
-def delitems(lista,last_id):
-	result = func_del(lista,last_id)
-	return result
-
-	
-@eel.expose
-def check(users,table_obj,table_crit,last_id):
-	result = func_check(users,table_obj,table_crit,last_id)
-	return result
+    result = func_load()
+    return result
 
 
 @eel.expose
-def run(table_obj,table_crit,users):
-	result = func_run(table_obj,table_crit,int(users))
-	
-	return result
+def delitems(lista, last_id):
+    result = func_del(lista, last_id)
+    return result
+
 
 @eel.expose
-def summary(values_list,likert_table_obj,users):
-	
-	result = func_summary(values_list,likert_table_obj,users)
+def check(users, table_obj, table_crit, last_id):
+    result = func_check(users, table_obj, table_crit, last_id)
+    return result
 
-	return result
-	
+
+@eel.expose
+def run(table_obj, table_crit, users):
+    result = func_run(table_obj, table_crit, int(users))
+
+    return result
+
+
+@eel.expose
+def summary(values_list, likert_table_obj, users):
+
+    result = func_summary(values_list, likert_table_obj, users)
+
+    return result
+
+
+@eel.expose
+def convert(dictio):
+    print('saving')
+    for element in dictio:
+        print('saving'+str(element))
+        file = open(str(element) + '.txt', 'w')
+        text = ""
+        for dictionary in dictio[element]:
+            for key in dictionary:
+                text = text + str(dictionary[key]) + '\n'
+        file.write(text)
+        file.close()
+    print('saved')
 
 eel.start('likert1.html')
